ec7f238c13c5ef5a4484f6ee41a50375292ccf73
Myles Ma <kvntma@gmail.com>
2024-12-25 17:07:37 -0500

    check

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Dlw3ZWUvh/4G7oGhVxVKs+cBc+wdSn72RvdIXu2uYRE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
